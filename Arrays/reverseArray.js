819196688a593cb37e61f668558bea9b69c4f188
Georgiy Garenov <g_garenov@outlook.com>
2023-02-01 21:27:20 +0200

    Arrays exercises

--- Arrays/reverseArray.js
+++ Arrays/reverseArray.js
@@ -1,3 +1,10 @@
-function reverseNumber(num) {
-    
+function reverseNumber(n, arr) {
+    let newArr = [];
+    for (let i = n - 1; i >= 0; i--) {
+        const element = arr[i];
+        newArr.push(element);
+
+    }
+    console.log(newArr.join(' '));
 }
+reverseNumber(3, [10, 20, 30, 40])
---
+++ Arrays/reverseInPlace.js
@@ -0,0 +1,9 @@
+function reverseInPlace(arr) {
+    for (let index = 0; index < arr.length; index++) {
+        let firstElement = arr[i];
+        let lastElement = arr[arr.length - 1 - i];
+
+        firstElement = lastElement
+        
+    }
+}
